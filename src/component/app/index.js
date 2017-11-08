341a587df4c3180087af1185f0c925dff2ecf179
Duncan Marsh <dwolfm@gmail.com>
2017-11-07 22:22:07 -0800

    store is working

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -1,5 +1,11 @@
-import * as util from '../../../engine/util.js'
+
 import * as cpu from '../../../engine/cpu.js'
+import * as util from '../../../engine/util.js'
+import * as memory from '../../../engine/memory.js'
+
+window.cpu = cpu
+window.util = util
+window.memory = memory
 
 class App extends React.Component {
   render(){
